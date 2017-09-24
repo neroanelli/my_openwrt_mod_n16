745d30e3c0c93a7eb9b9495d69cf06529f7f387b
neroanelli <neroanelli@gmail.com>
2017-09-24 17:46:09 +0800

    update

--- newpackage/luci-app-shadowsocksr-pro/root/etc/shadowsocksr/up-gfwlist.sh
+++ newpackage/luci-app-shadowsocksr-pro/root/etc/shadowsocksr/up-gfwlist.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 /etc/shadowsocksr/gen-gfwlist.sh > /tmp/ol-gfw.txt
-
+flag=1
 if [ -s "/tmp/ol-gfw.txt" ];then
 	sort -u /etc/shadowsocksr/base-gfwlist.txt /tmp/ol-gfw.txt > /tmp/china-banned
 	if ( ! cmp -s /tmp/china-banned /etc/gfwlist/china-banned );then
 		if [ -s "/tmp/china-banned" ];then
 			mv /tmp/china-banned /etc/gfwlist/china-banned
 			echo "Update GFW-List Done!"
+			flag=0
 		fi
 	else
 		echo "GFW-List No Change!"
+		flag=1
 	fi
 fi
 
 rm -f /tmp/gfwlist.txt
 rm -f /tmp/ol-gfw.txt
-
-/etc/init.d/ssrpro restart
-
+[ "$flag" = 0 ] && /etc/init.d/ssrpro restart
